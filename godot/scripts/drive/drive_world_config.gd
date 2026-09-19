ab499c08e4351bdc40c3687c101302ef3fd6f9e4
patrickcallahancomedy <patrickcallahancomedy@gmail.com>
2026-09-19 19:30:46 -0400

    Define physical spacing for DRIVE regions

--- godot/scripts/drive/drive_world_config.gd
+++ godot/scripts/drive/drive_world_config.gd
@@ -1,16 +1,8 @@
 class_name DriveWorldConfig
 extends RefCounted
 
-## DRIVE world layout.
-##
-## The game uses one continuous 20x20 road world.
-## Four 5x5 regions are placed inside that world and physically connected:
-## neighborhood -> highway -> city -> parking lot.
-##
-## Main view and minimap both render this same world data.
-
-
 const WORLD_SIZE := 20
+const MAP_SIZE := Vector2(42.0, 34.0)
 
 const START := Vector2i(0, 16)
 
@@ -19,6 +11,8 @@ const REGIONS := {
 		"name": "NEIGHBORHOOD",
 		"origin": Vector2i(0, 12),
 		"size": Vector2i(5, 5),
+		"physical_origin": Vector2(2.0, 21.0),
+		"spacing": 2.6,
 		"speed_limit": 25,
 		"road_type": "neighborhood",
 	},
@@ -26,6 +20,8 @@ const REGIONS := {
 		"name": "HIGHWAY",
 		"origin": Vector2i(5, 8),
 		"size": Vector2i(5, 5),
+		"physical_origin": Vector2(15.8, 7.4),
+		"spacing": 3.4,
 		"speed_limit": 65,
 		"road_type": "highway",
 	},
@@ -33,6 +29,8 @@ const REGIONS := {
 		"name": "CITY",
 		"origin": Vector2i(10, 4),
 		"size": Vector2i(5, 5),
+		"physical_origin": Vector2(31.4, 8.2),
+		"spacing": 1.0,
 		"speed_limit": 30,
 		"road_type": "city",
 	},
@@ -40,6 +38,8 @@ const REGIONS := {
 		"name": "PARKING LOT",
 		"origin": Vector2i(15, 4),
 		"size": Vector2i(5, 5),
+		"physical_origin": Vector2(37.4, 8.2),
+		"spacing": 0.8,
 		"speed_limit": 10,
 		"road_type": "parking",
 	},
